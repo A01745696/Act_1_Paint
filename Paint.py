0a173bfd461d727bc5ced646973792464d3d67db
Fabrizio González <A01751578@itesm.mx>
2021-05-06 12:07:37 -0500

    Fabrizio agrega función de circulo y nuevo color

--- Paint.py
+++ Paint.py
@@ -32,29 +32,33 @@ def square(start, end):
         left(90)
 
     end_fill()
+    
 
 def circulo(start, end):
     
-     """
+    """
     [Draw circle]
     :parametrer start: [Values of the first click in x and y]
     :parameter ends: [Values of the second click in x and y]
     """
     
-    up()
+    up() 
     goto(start.x, start.y)
     down()
     begin_fill()
-    circle((end.x - start.x)/2, 360)
+    circle((end.x - start.x)/2)
     end_fill()
+    
     
 def rectangle(start, end):
     "Draw rectangle from start to end."
     pass  # TODO
 
+
 def triangle(start, end):
     "Draw triangle from start to end."
     pass  # TODO
+
 
 def tap(x, y):
     "Store starting point or draw shape."
@@ -67,25 +71,32 @@ def tap(x, y):
         end = vector(x, y)
         shape(start, end)
         state['start'] = None
+        
 
 def store(key, value):
     "Store value in state at key."
     state[key] = value
-
+    
+# Define una pantalla de dimensiones 420x420 ubicada en x=2320 y y=0
 state = {'start': None, 'shape': line}
 setup(420, 420, 370, 0)
 onscreenclick(tap)
-listen()
-onkey(undo, 'u')
+listen()             # Escucha los eventos realizados por el teclado
+onkey(undo, 'u')     # Elimina la ultima acción realizada
+
+# Define las teclas para cambiar los colores
 onkey(lambda: color('black'), 'K')
 onkey(lambda: color('white'), 'W')
 onkey(lambda: color('green'), 'G')
 onkey(lambda: color('blue'), 'B')
 onkey(lambda: color('red'), 'R')
 onkey(lambda: color('violet'), 'V')
+
+# Define las teclas para cambiar las figuras
 onkey(lambda: store('shape', line), 'l')
 onkey(lambda: store('shape', square), 's')
 onkey(lambda: store('shape', circulo), 'c')
 onkey(lambda: store('shape', rectangle), 'r')
 onkey(lambda: store('shape', triangle), 't')
+
 done()
